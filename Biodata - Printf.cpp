984f0c4923d5a11ea73b7a636898167039a08103
Star Labs <55492779+starlabs-id@users.noreply.github.com>
2020-10-15 20:10:18 +0800

    Update Biodata - Printf.cpp

--- Biodata - Printf.cpp
+++ Biodata - Printf.cpp
@@ -4,11 +4,11 @@
 
 void main() {
 	clrscr();
-	printf ("Nama	: Kevin R \n");
-	printf ("NIM	: 150010310 \n");
-	printf ("Alamat : Jl. Gn. Talang IIa No. 4 Denpasar \n");
+	printf ("Nama	: Starlabs \n");
+	printf ("NIM	: 1700 \n");
+	printf ("Alamat : Jl. Sehat \n");
 	printf ("Umur	: 21 \n");
-	printf ("TTL	: Malang, 2 Juli 1994 \n");
+	printf ("TTL	: Sidney, 1 Oktober 2000 \n");
 	printf ("Hobi	: Baca Koran \n");
-	printf ("No.HP	: 085739065927");
+	printf ("No.HP	: 085739065887");
 }
